6853ac05ee2e3f76474591e958341238a7535ab4
Sandhya Pandey <168643341+sandhya144@users.noreply.github.com>
2025-08-15 17:08:22 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -48,7 +48,7 @@ const Hero = () => {
 
       {/* resume button */}
       <a
-      //  href="https://drive.google.com/file/d/11fS4gjjUbAHBfZJWcZXmZo8opfKp0wEd/view"
+      //  href="https://drive.google.com/file/d/S4gjjUbAHBfZJWcZXmZo8opfKp0wEd/view"
        target="_blank"
        rel="noopener noreferrer"
        className="inline-block text-white py-3 px-8 rounded-full mt-8 text-lg font-bold transition duration-300 transform hover:scale-105"
